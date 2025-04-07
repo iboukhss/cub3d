8f5568aed046ac0d5a0a16bc905ac76b378ec968
Heck Dennis <heckdennis257@gmail.com>
2025-04-07 21:52:36 +0200

    feat: adding ft_strncmp.c

---
+++ libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dheck <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 18:40:51 by dheck             #+#    #+#             */
+/*   Updated: 2024/03/19 14:40:49 by dheck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdint.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+#endif

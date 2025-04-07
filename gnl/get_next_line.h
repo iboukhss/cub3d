5bf2f2774ed3509ddcf8acb88c7a05e429a82006
Heck Dennis <heckdennis257@gmail.com>
2025-04-07 21:51:47 +0200

    feat: adding a gnl version

---
+++ gnl/get_next_line.h
@@ -0,0 +1,22 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1
+#endif
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char *get_next_line(int fd);
+char *ft_strdup(char *s);
+char *ft_strjoin(char *s1, char *s2);
+char *ft_strchr(char *s, char c);
+char *ft_substr(char *s, int start, int len);
+size_t ft_strlen(char *s);
+void ft_memcpy(char *dst, char *src, int n);
+
+#endif

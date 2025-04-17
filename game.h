4f604494eaa8f886167ba4a6d08044d05f492241
Ismail <iboukhss@student.42luxembourg.lu>
2025-04-17 11:35:03 +0200

    rebase: fix game.h header

--- game.h
+++ game.h
@@ -6,7 +6,7 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 19:19:47 by iboukhss          #+#    #+#             */
-/*   Updated: 2025/04/11 17:40:14 by iboukhss         ###   ########.fr       */
+/*   Updated: 2025/04/17 11:33:22 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@
 # define WIN_WIDTH 1280
 # define WIN_HEIGHT 800
 # define TILE_SIZE 24
+
+# define MAX_GRID_SIZE 255
+
+#define WHITESPACE " "
+#define IDENTIFIER "NOEAWESOFC"
 
 # include "graphics.h"
 # include "vec2.h"
@@ -36,9 +41,11 @@ enum e_orientation
 // also be great also. Holes in the map could be padded with spaces for example.
 typedef struct s_map
 {
-	char	**grid;
+	char	*scene_path;
+	char	*grid[MAX_GRID_SIZE];
 	int		width;
 	int		height;
+	char	spawn_orientation;
 }	t_map;
 
 typedef struct s_camera
@@ -59,10 +66,23 @@ typedef struct s_player
 	t_camera			cam;
 }	t_player;
 
+typedef struct s_rgb
+{
+	int	red;
+	int	green;
+	int	blue;
+}	t_rgb;
+
 // This should hold the texture configuration data. Didn't look at this yet.
 typedef struct s_config
 {
-	// Add configuration data here
+	char	*NO;
+	char	*SO;
+	char	*WE;
+	char	*EA;
+	t_rgb	*floor;
+	t_rgb	*ceiling;
+	int 	done;
 }	t_config;
 
 // Main game structure
@@ -75,7 +95,7 @@ typedef struct s_game
 	t_window	win1;
 	t_map		map;
 	t_player	player;
-	t_config	conf;
+	t_config	config;
 }	t_game;
 
 int	init_game(t_game *game);
@@ -90,5 +110,15 @@ int	render_scene(void *param);
 int	keyrelease_main(int keysym, void *param);
 int	keyrelease_debug(int keysym, void *param);
 int	keypress_main(int keysym, void *param);
+
+// Newly added
+char	*get_next_line(int fd);
+char	*skip_whitespace(char *line);
+int		extract_param(t_config *config, char *line);
+int		get_scene(t_game *game, char *scene_path);
+int		read_scene(t_game *game, char *scene_path);
+void	print_error(int default_prompt, char *error_msg);
+void	print_config(t_config config);
+void	print_map(t_map map);
 
 #endif // GAME_H

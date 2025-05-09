df400915d5a7d155554f6bb64102a566c68ff768
Heck Dennis <heckdennis257@gmail.com>
2025-05-09 15:57:26 +0200

    feature: handle spaces in rgb color code

---
+++ extract_element.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   extract_element.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dennis <dennis@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/09 11:35:28 by dennis            #+#    #+#             */
+/*   Updated: 2025/05/09 15:56:38 by dennis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "game.h"
+#include "libft.h"
+#include "get_next_line.h"
+
+char	*get_element(char *line, char **element)
+{
+	char	*bgn;
+
+	bgn = line;
+	while (*line != '\n' && ft_strchr(WHITESPACE, *line) == NULL
+		&& ft_strchr("\n", *line) == NULL)
+		line++;
+	*element = ft_substr(bgn, 0, (line - bgn));
+	if (!(*element))
+	{
+		print_error(1, "Invalid Scene. Missing identifier or path.");
+		return (NULL);
+	}
+	return (line);
+}
+
+char	*get_rgb_code(char *line, char **element)
+{
+	char	*bgn;
+
+	bgn = line;
+	while (*line != '\n')
+		line++;
+	*element = ft_substr(bgn, 0, (line - bgn));
+	if (!(*element))
+	{
+		print_error(1, "Invalid Scene. Missing rgb code.");
+		return (NULL);
+	}
+	return (line);
+}

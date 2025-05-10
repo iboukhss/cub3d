5ee165b2f841ae0501c2bb2b28b548a8deb63e39
Heck Dennis <heckdennis257@gmail.com>
2025-05-10 10:45:56 +0200

    feature: adding checks to detect and handle tabs

--- extract_element.c
+++ extract_element.c
@@ -6,7 +6,7 @@
 /*   By: dennis <dennis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 11:35:28 by dennis            #+#    #+#             */
-/*   Updated: 2025/05/09 17:08:56 by dennis           ###   ########.fr       */
+/*   Updated: 2025/05/10 10:40:42 by dennis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,16 @@ char	*get_element(char *line, char **element)
 	char	*bgn;
 
 	bgn = line;
-	while (*line != '\n' && ft_strchr(WHITESPACE, *line) == NULL
-		&& ft_strchr("\n", *line) == NULL)
+	while (*line != '\n' && ft_strchr(WHITESPACE, *line) == NULL)
+	{
+		if (ft_strchr(TABS, *line) != NULL)
+		{
+			*element = NULL;
+			print_error(1, "Invalid scene. Only spaces allowed.");
+			return (NULL);
+		}
 		line++;
+	}
 	*element = ft_substr(bgn, 0, (line - bgn));
 	if (!(*element))
 	{
@@ -37,7 +44,15 @@ char	*get_rgb_code(char *line, char **element)
 
 	bgn = line;
 	while (*line != '\n')
+	{
+		if (ft_strchr(TABS, *line) != NULL)
+		{
+			*element = NULL;
+			print_error(1, "Invalid scene. Only spaces allowed.");
+			return (NULL);
+		}
 		line++;
+	}
 	*element = ft_substr(bgn, 0, (line - bgn));
 	if (!(*element))
 	{
